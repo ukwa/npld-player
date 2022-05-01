da85096fe46817e7c0e02cf529a0686010a8a5e5
sua yoo <sua@suayoo.com>
2022-04-30 22:20:38 -0700

    temporarily disable csp

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import { app, BrowserWindow } from 'electron';
 // whether you're running in development or production).
 declare const MAIN_WINDOW_WEBPACK_ENTRY: string;
 
+// const isDev = !app.isPackaged;
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
   // eslint-disable-line global-require
