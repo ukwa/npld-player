53254bea6a2588fe5b0ee4bb333db9c635c55723
sua yoo <sua@suayoo.com>
2022-04-30 22:02:21 -0700

    switch to webpack

--- src/app.ts
+++
@@ -1,6 +0,0 @@
-import '@shoelace-style/shoelace/dist/themes/light.css';
-import '@shoelace-style/shoelace/dist/shoelace.js';
-import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.js';
-
-// Set the base path to the folder you copied Shoelace's assets to
-setBasePath('dist/shoelace');
---
+++ src/renderer.ts
@@ -0,0 +1,14 @@
+/**
+ * This file will automatically be loaded by webpack and run in the "renderer" context.
+ * To learn more about the differences between the "main" and the "renderer" context in
+ * Electron, visit:
+ *
+ * https://electronjs.org/docs/tutorial/application-architecture#main-and-renderer-processes
+ */
+import '@shoelace-style/shoelace/dist/themes/light.css';
+import '@shoelace-style/shoelace/dist/shoelace.js';
+import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.js';
+import './index.css';
+
+// Set the base path to the folder you copied Shoelace's assets to
+setBasePath('public/shoelace');
