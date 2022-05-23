5017962af82419900caaeb4617276fc76d52609b
sua yoo <sua@suayoo.com>
2022-05-23 15:18:20 -0700

    Expose custom environment variables (#21)

--- src/preload.ts
+++ src/preload.ts
@@ -1,12 +1,3 @@
-import dotenv from 'dotenv';
-
-// Read from .env file
-dotenv.config();
-
-const { NPLD_PLAYER_INITIAL_WEB_ADDRESS } = process.env;
-
 window.addEventListener('DOMContentLoaded', () => {
-  const npldPlayer = document.querySelector('npld-player');
-
-  npldPlayer.setAttribute('url', NPLD_PLAYER_INITIAL_WEB_ADDRESS);
+  // const npldPlayer = document.querySelector('npld-player') as any;
 });
