72c4f582a6b1f2c19b27c798765698e682499268
Andy Jackson <Andrew.Jackson@bl.uk>
2023-03-02 12:44:45 +0000

    Update shoelace setup.

--- src/renderer.ts
+++ src/renderer.ts
@@ -6,11 +6,21 @@
  * https://electronjs.org/docs/tutorial/application-architecture#main-and-renderer-processes
  */
 declare const MAIN_WINDOW_WEBPACK_ENTRY: string;
-import '@shoelace-style/shoelace/dist/shoelace.js';
+
+// Shoelace Setup
+// https://shoelace.style/getting-started/installation
+import '@shoelace-style/shoelace/dist/themes/light.css';
+import '@shoelace-style/shoelace/dist/components/button/button.js';
+import '@shoelace-style/shoelace/dist/components/icon/icon.js';
+import '@shoelace-style/shoelace/dist/components/input/input.js';
+import '@shoelace-style/shoelace/dist/components/rating/rating.js';
 import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.js';
-import './index.css';
-import './npld-player';
 
 // Set the base path to the folder you copied Shoelace's assets to
 const isLocalFile = window.location.origin === 'file://';
 setBasePath(`${isLocalFile ? '.' : window.location.href}/public/shoelace`);
+
+// App imports
+import './index.css';
+import './npld-player';
+
