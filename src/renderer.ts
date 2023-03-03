63721eeacbc31179a17808e42f50278da3eaf64b
Andy Jackson <Andrew.Jackson@bl.uk>
2023-03-03 16:45:16 +0000

    Include required elements

--- src/renderer.ts
+++ src/renderer.ts
@@ -13,7 +13,7 @@ import '@shoelace-style/shoelace/dist/themes/light.css';
 import '@shoelace-style/shoelace/dist/components/button/button.js';
 import '@shoelace-style/shoelace/dist/components/icon/icon.js';
 import '@shoelace-style/shoelace/dist/components/input/input.js';
-import '@shoelace-style/shoelace/dist/components/rating/rating.js';
+import '@shoelace-style/shoelace/dist/components/icon-button/icon-button.js';
 import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.js';
 
 // Set the base path to the folder you copied Shoelace's assets to
