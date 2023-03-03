3333591ca82b857868ec67e76fae52fa00004666
Andy Jackson <Andrew.Jackson@bl.uk>
2023-03-03 00:12:28 +0000

    Add runtime override

--- webpack.plugins.js
+++ webpack.plugins.js
@@ -7,11 +7,11 @@ const CopyPlugin = require('copy-webpack-plugin');
 require('dotenv').config();
 
 module.exports = [
-  // Make env variables available with `process.env.ENV_VARIABLE`
+  // Make compile-time env variables available with `process.env.ENV_VARIABLE`
   new webpack.EnvironmentPlugin([
     // Avoid hardcoding these configuration parameters:
-    //'NPLD_PLAYER_PREFIX',
-    //'NPLD_PLAYER_INITIAL_WEB_ADDRESS',
+    'NPLD_PLAYER_PREFIX',
+    'NPLD_PLAYER_INITIAL_WEB_ADDRESS',
     'NPLD_PLAYER_AUTH_TOKEN_NAME',
     'NPLD_PLAYER_AUTH_TOKEN_VALUE',
     'NPLD_PLAYER_ENABLE_PRINT',
